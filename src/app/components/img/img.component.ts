5ff1feea701c6d8b1a03cdfa0272373c57266c71
juan carlos <juancarlosmoreno356@gmail.com>
2022-10-30 22:30:45 -0500

    si otra vez xd

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,20 +1,43 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
   imageDefault = './assets/images/default.png';
 
-  constructor() { }
+  constructor() {
+    // before render
+    // NO async -- once time
+    console.log('constructor', 'imgValue =>', this.img);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // before render
+    // changes inputs -- times
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+  }
 
   ngOnInit(): void {
+    // before render -
+    console.log('ngOnInit', 'imgValue =>', this.img);
   }
+
+  ngAfterViewInit(): void {
+    // aster render
+    // handler children
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
   imgError() {
     this.img = this.imageDefault;
   }
